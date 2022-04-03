1ad19db019634ff70df7c099c6b45a3673147077
SiDraw <sidraw.fr@gmail.com>
2022-04-03 20:10:48 +0800

    [Build] support class validator

---
+++ src/decorator/validators/match.decorator.ts
@@ -0,0 +1,38 @@
+/* eslint-disable class-methods-use-this */
+import { ClassConstructor } from 'class-transformer';
+import {
+  registerDecorator,
+  ValidationArguments,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
+
+@ValidatorConstraint({ name: 'Match' })
+export class MatchConstraint implements ValidatorConstraintInterface {
+  validate(value: any, args: ValidationArguments) {
+    const [fn] = args.constraints;
+    return fn(args.object) === value;
+  }
+
+  defaultMessage(args: ValidationArguments) {
+    const [constraintProperty]: (() => any)[] = args.constraints;
+    return `${constraintProperty} and ${args.property} does not match`;
+  }
+}
+
+export const Match = <T>(
+  type: ClassConstructor<T>,
+  property: (o: T) => any,
+  validationOptions?: ValidationOptions,
+) => {
+  return (object: any, propertyName: string) => {
+    registerDecorator({
+      target: object.constructor,
+      propertyName,
+      options: validationOptions,
+      constraints: [property],
+      validator: MatchConstraint,
+    });
+  };
+};
